911ff1de774f2db22d2436cb0448144d27b4af48
Vasu Adari <vasuakeel@gmail.com>
2020-03-30 23:49:15 +0530

    Add support for Singapore time zone

--- lib/mix/tasks/hacker_news_alert.ex
+++ lib/mix/tasks/hacker_news_alert.ex
@@ -3,28 +3,70 @@ defmodule Mix.Tasks.HackerNewsAlert do
 
   @app_name :hacker_news_alert
 
-  def run(_) do
+  def run([time_zone_arg]) do
     {:ok, _} = Application.ensure_all_started(@app_name)
     news_items = HackerNews.fetch_top_news()
 
-    Path.join([
-      :code.priv_dir(@app_name),
-      "static",
-      "subscribers.txt"
-    ])
+    subscribers_file_path()
     |> File.stream!()
-    |> Stream.each(fn row ->
-      [name, email] = String.split(row, ",")
+    |> Stream.each(fn
+      "\n" ->
+        :skip
 
-      {:ok, _} =
-        HackerNews.send_top_news_alert(
+      "#" <> _ ->
+        :skip
+
+      row ->
+        [name, email, time_zone] = String.split(row, ",")
+        name = String.trim(name)
+        email = String.trim(email)
+        time_zone = String.trim(time_zone)
+
+        send_alert(
+          Mix.env(),
           name,
-          String.trim(email),
+          email,
+          time_zone,
+          time_zone_arg,
           news_items
         )
+
+        :ok
     end)
     |> Stream.run()
 
     IO.puts("Emails have been sent successfully.")
+  end
+
+  defp send_alert(:prod, name, email, time_zone, input_time_zone, news_items)
+       when input_time_zone == time_zone do
+    {:ok, _} =
+      HackerNews.send_top_news_alert(
+        name,
+        String.trim(email),
+        news_items
+      )
+  end
+
+  defp send_alert(env, _name, email, time_zone, input_time_zone, _news_items)
+       when input_time_zone == time_zone do
+    IO.puts("Mock sending email to #{email} in #{time_zone} time_zone in #{env} env.")
+  end
+
+  defp send_alert(_env, _name, _email, _time_zone, _input_time_zone, _news_items), do: :ok
+
+  defp subscribers_file_path do
+    file_name =
+      if Mix.env() == :prod do
+        "subscribers.txt"
+      else
+        "test_subscribers.txt"
+      end
+
+    Path.join([
+      :code.priv_dir(@app_name),
+      "static",
+      file_name
+    ])
   end
 end
